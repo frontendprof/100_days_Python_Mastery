ccd9aae6a3264ef0dab5176b62583c6c03a9b97d
Abdumalik <48346947+python-pro@users.noreply.github.com>
2019-05-23 01:17:36 -0500

    Create Day_4_PMastery.py

---
+++ Day_4_PMastery.py
@@ -0,0 +1,13 @@
+# B_R_R
+# M_S_A_W
+
+# for given several numbers which are separated by commas we try to include them in list and then tuple.
+
+# Display them in list and tuple 
+
+
+num = input("Enter comma separated numbers: ")
+list=num.split(",")
+tuple= tuple(list)
+
+print(list, tuple)
